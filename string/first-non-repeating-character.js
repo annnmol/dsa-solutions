eeb9b8a0f15f8c3d4e5d61b4efa76730a69f5dec
annnmol <tanwaranmol6@gmail.com>
2025-08-24 12:43:05 +0530

    first unique character

---
+++ string/first-non-repeating-character.js
@@ -0,0 +1,31 @@
+/**
+ * @problem https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=problem-list-v2&envId=string
+ * @param {string} s
+ * @return {number}
+ */
+
+var solution = function (s) {
+  const map = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i];
+    if (map.has(ch)) {
+      map.set(ch, map.get(ch) + 1);
+    } else {
+      map.set(ch, 1);
+    }
+  }
+  console.log(map);
+
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i];
+    if (map.get(ch) === 1) return i;
+  }
+
+  return -1;
+};
+
+// ✅ Example usage
+console.log(solution("swiss")); // 1
+console.log(solution("abccbda")); // 5
+console.log(solution("abba")); // -1

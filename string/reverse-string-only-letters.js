831bf1e44f42500ea0a14c2c6b6358eff6d07f35
annnmol <tanwaranmol6@gmail.com>
2025-08-26 22:13:21 +0530

    reverse only letters added

---
+++ string/reverse-string-only-letters.js
@@ -0,0 +1,36 @@
+/**
+ * @problem https://leetcode.com/problems/reverse-only-letters/
+ * @param {string} s
+ * @return {string}
+ */
+function isCharacter(ch) {
+    let code = ch.charCodeAt(0);
+    // a-z 97-122 A-Z 65-90
+    return (code >= 65 && code <= 90) || (code >= 97 && code <= 122)
+}
+var solution = function (s) {
+     let arr = s.split('');
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+
+        while (left < right && !isCharacter(arr[left])) {
+            left++;
+        }
+        while (left < right && !isCharacter(arr[right])) {
+            right--;
+        }
+        if (left < right) {
+            let temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--
+        }
+    }
+    return arr.join('');
+};
+
+// ✅ Example usage
+console.log(solution("a-bC-dEf-ghIj")); // j-Ih-gfE-dCba
+console.log(solution("Test1ng-Leet=code-Q!")); //Qedo1ct-eeLg=ntse-T!
--- string/reverse-string-vowels.js
+++ string/reverse-string-vowels.js
@@ -5,29 +5,49 @@
  */
 
 var solution = function (s) {
-  const constants = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  let vowels = [];
-  let modifiedString = [];
-  for (const i of s) {
-    if (constants.includes(i)) {
-      vowels.push(i);
-      modifiedString.push("*");
-    } else {
-      modifiedString.push(i);
+  // const constants = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  // let vowels = [];
+  // let modifiedString = [];
+  // for (const i of s) {
+  //   if (constants.includes(i)) {
+  //     vowels.push(i);
+  //     modifiedString.push("*");
+  //   } else {
+  //     modifiedString.push(i);
+  //   }
+  // }
+  // const reversedVowels = vowels.reverse();
+  // let res = [];
+  // let index = 0;
+  // for (const i of modifiedString) {
+  //   if (i === "*") {
+  //     res.push(reversedVowels[index]);
+  //     index++;
+  //   } else {
+  //     res.push(i);
+  //   }
+  // }
+  // return res.join("");
+  let vowels = new Set(["a","e","i","o","u","A","E","I","O","U"]);
+  let arr = s.split("");
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    while (left < right && !vowels.has(arr[left])) {
+      left++;
+    }
+    while (left < right && !vowels.has(arr[right])) {
+      right--;
+    }
+    if (left < right) {
+      let temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+      left++;
+      right--;
     }
   }
-  const reversedVowels = vowels.reverse();
-  let res = [];
-  let index = 0;
-  for (const i of modifiedString) {
-    if (i === "*") {
-      res.push(reversedVowels[index]);
-      index++;
-    } else {
-      res.push(i);
-    }
-  }
-  return res.join("");
+  return arr.join("");
 };
 
 // ✅ Example usage
